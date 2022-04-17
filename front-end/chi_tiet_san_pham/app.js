1c820b2d2eb9ae384742a4e9bb04739960fec40d
DangHuynh <danghuynh.dev@gmail.com>
2022-04-17 22:56:35 +0700

    design page detail produce

--- front-end/chi_tiet_san_pham/app.js
+++ front-end/chi_tiet_san_pham/app.js
@@ -12,3 +12,23 @@ $("#btn-pre").click(function(){
 $("#btn-next").click(function(){
     $('.list-img-item').scrollLeft( $('.list-img-item').scrollLeft() + 86 );
 })
+
+$('.amount-input').on('keypress', function (event) {
+    var charCode = !event.charCode ? event.which : event.charCode;
+    if( charCode == 46 || charCode == 69 || charCode == 101 
+    || charCode == 45 || charCode == 43)
+        event.preventDefault();
+});
+$('.amount-input').blur(function(){
+    if($(this).val()==""||$(this).val()==0)
+        $(this).val(1);
+})
+$(".btn-sub").on("click", function(){
+    let value = Number($('.amount-input').val());
+    if(value==0) return;
+    $('.amount-input').val(value-1);
+})
+$(".btn-add").on("click", function(){
+    let value = Number($('.amount-input').val());
+    $('.amount-input').val(value+1);
+})
