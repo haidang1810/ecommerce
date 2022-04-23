90cc9d0eea419ddf40b7168220d1d217208f9efd
DangHuynh <danghuynh.dev@gmail.com>
2022-04-23 08:19:46 +0700

    complete design add order

--- front-end/admin/don_hang/them_don_hang/app.js
+++ front-end/admin/don_hang/them_don_hang/app.js
@@ -24,3 +24,9 @@ $(".btn-add").on("click", function(){
     let value = Number($('.amount-input').val());
     $('.amount-input').val(value+1);
 })
+$('#phone-add-customer').on('keypress', function (event) {
+    var charCode = !event.charCode ? event.which : event.charCode;
+    if( charCode == 46 || charCode == 69 || charCode == 101 
+    || charCode == 45 || charCode == 43)
+        event.preventDefault();
+});
