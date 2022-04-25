b4bc008e462cb5b89c73d14d8abc854a51a01621
DangHuynh <danghuynh.dev@gmail.com>
2022-04-25 22:10:27 +0700

    complete design page campaign

---
+++ front-end/admin/chien_dich/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#table-campaign').DataTable({
+        "lengthMenu": [10, 15, 20, 25, 30, 40, 50 ]
+    });
+});
---
+++ front-end/admin/chien_dich/them_chien_dich/app.js
@@ -0,0 +1,6 @@
+$('.discount__input--number').on('keypress', function (event) {
+    var charCode = !event.charCode ? event.which : event.charCode;
+    if( charCode == 46 || charCode == 69 || charCode == 101 
+    || charCode == 45 || charCode == 43)
+        event.preventDefault();
+});
