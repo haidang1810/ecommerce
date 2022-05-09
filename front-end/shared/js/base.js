d159e3167e378007d9209908a8d1cc8025762638
DangHuynh <danghuynh.dev@gmail.com>
2022-05-09 21:54:48 +0700

    fix bug click logo, search btn

--- front-end/shared/js/base.js
+++ front-end/shared/js/base.js
@@ -15,7 +15,7 @@ function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 $("#logo-link").click(function(){
-	window.location.href = BASE_URL_CLIENT+'/';
+	window.location.href = BASE_URL_CLIENT;
 })
 function renderStarRating(totalStar){
 	let html = ``;
