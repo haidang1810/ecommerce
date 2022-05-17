62bad69277e20791af989c731c19597a6c15d2c1
DangHuynh <danghuynh.dev@gmail.com>
2022-05-17 13:59:21 +0700

    fix bug update address

--- api/src/app/controllers/AddressesController.js
+++ api/src/app/controllers/AddressesController.js
@@ -15,19 +15,41 @@ class AddressesController {
 		})
     }
 	updateByCustomer(req,res){
-		Address.updateOne({customer: req.body.customer},req.body)
-			.then(()=>{
-				res.json({
-					status: 1,
-					msg: 'success !!!'
-				});	
+		Address.findOne({customer: req.body.customer})
+			.then((data)=>{
+				if(data){
+					Address.updateOne({customer: req.body.customer},req.body)
+					.then(()=>{
+						res.json({
+							status: 1,
+							msg: 'success !!!'
+						});	
+					})
+					.catch(()=>{
+						res.json({
+							status: 0,
+							msg: 'Có lỗi vui lòng thử lại !!!'
+						});	
+					})
+				}else{
+					const address =new Address(req.body);
+					address.save()
+                            .then(() => {
+                                res.json({
+									status: 1,
+									msg: 'success !!!'
+								});	
+                            })
+							.catch(()=>{
+								res.json({
+									status: 0,
+									msg: 'Có lỗi vui lòng thử lại !!!'
+								});
+							})
+				}
+				
 			})
-			.catch(()=>{
-				res.json({
-					status: 0,
-					msg: 'Có lỗi vui lòng thử lại !!!'
-				});	
-			})
+		
 	}
 }
 module.exports = new AddressesController();
